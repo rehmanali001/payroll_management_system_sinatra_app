e239b54d11a242c0aa3eaabb4f7fab5e263ab6d2
Rehman Ali <rehmanali001@gmail.com>
2019-09-26 15:03:15 +0000

    temp changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,34 @@
 class UsersController < ApplicationController
 
-    get '/signup' do 
+    get '/signup' do
         erb :"users/signup"
     end
 
-    post '/signup' do 
-        user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    post '/signup' do
+        user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
        if user.save
-          redirect '/employees'
-       else  
-        redirect '/' 
-    end 
+          redirect '/login'
+       else
+        redirect '/'
+    end
     end
 
-    get '/login' do 
-        
-        erb :"users/login"
-    end 
+    get '/login' do
 
-    post '/login' do 
+        erb :"users/login"
+    end
+
+    post '/login' do
         @user = User.find_by(email: params[:email], password: params[:password])
         if @user && user.authenticate(params[:password])
-            session[:user_id] = @user.id 
+            session[:user_id] = @user.id
             redirect '/employees'
-        end 
+        end
         redirect '/login'
     end
 
-    get '/logout' do 
-        session.clear 
+    get '/logout' do
+        session.clear
         redirect '/'
     end
-end 
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
     has_secure_password
     has_many :employees
-  #  validates_presence_of :username, :email, :password
+    validates_presence_of :username, :email, :password
 
 end     
